2c37f21c966914d798a0fa4a2f2f61e3684fc346
prince-arun <prince.skillsafari@gmail.com>
2023-04-14 23:46:47 +0530

    Arrays basics

--- 02. javaScript/1.self practice/B - Fundamentals/04-Arrays & String methods/script.js
+++ 02. javaScript/1.self practice/B - Fundamentals/04-Arrays & String methods/script.js
@@ -15,6 +15,23 @@ var myArray2 = ["apple", ["mac", "mac pro", [2017, 2020]]];
 
 console.log(myArray2[1][2][0]);
 console.log(myArray2.indexOf("mac pro"));
+console.log("======================================================");
+
+//We can also add objects and functions to the arrays.
+let arrOne = [
+  "name",
+  { age: 23 },
+  function () {
+    return console.log("say hai");
+  },
+];
+console.log(arrOne[0]); //accessing array elements using bracket notation.
+console.log(arrOne[1].age); //to access a object we use a bracket notation followed by dot notation.
+console.log(arrOne[2]()); //we can call the function in the arrays using bracket notation followed by brackets.
+arrOne[2]();
+
+console.log("=====================================================");
+//------------------------------------------------------------------------------------------------------
 
 //Array-functions
 //push    Adds string/int/boo/float at last of the array.
@@ -32,3 +49,20 @@ console.log(myArray);
 //shift     Remove string/int/boo/float at first of the array.
 myArray.shift("musk");
 console.log(myArray);
+
+//---------------------------------------------------------------------------------------------------
+//for...of loop
+let arrTwo = ["name", 34, true];
+for (let key1 of arrTwo) {
+  console.log(key1);
+}
+
+//Array length property
+let arrThree = [1, 2, 3, 4, 5];
+console.log(arrThree.length);
+
+arrThree.length = [3];
+console.log(arrThree); //Here we shorten the arrays length by three, so it shows only three elements.
+
+arrThree.length = [5];
+console.log(arrThree); //Here also gives only three, because shorteninig the arry is irreversable, we can shorten the length but we cant revive it to ist older form.
