fcd5d7a77922b7e3b18e8f639e53eb26d9d68a7e
prince-arun <prince.skillsafari@gmail.com>
2023-05-31 10:45:15 +0530

    API - Intro

---
+++ 02. javaScript/k - API/01 - HTTP/Intro.js
@@ -0,0 +1,32 @@
+//HTTP - Intro
+//1. Request Methods
+// GET, POST, PUT/PATCH, DELETE
+
+//2. Status code
+// 200+ => successful
+// 300+ => Redirection code
+// 400+ => client errors
+// 500+ => internal / server errors
+
+//3.Headers
+//Additional information about the requests.
+//It stores in key-value pair.
+// Authorization Headers
+// content-type
+// cache - control
+// Accept headers
+
+//4.Messages Format
+//Two types : Request and Response
+//i ) Request => request line, headers, body
+//II) Response => status line, headers, body
+
+//HTTP - Methods
+//1.GET
+//2.POST
+//3.PUT/PATCH
+//4.DELETE
+//5.OPTIONS
+//6.HEAD
+//7.CONNECT
+//8.TRACE
