20b26cdc323698411deced50ac8460a01a64e319
prince-arun <prince.skillsafari@gmail.com>
2023-07-05 08:12:09 +0530

    list - array of object

--- 04.React/01-Intro/intro-app/src/App.js
+++ 04.React/01-Intro/intro-app/src/App.js
@@ -10,6 +10,24 @@ const nm = "prince";
 const welcome = (msg) => {
   return `Hello ${msg}`;
 };
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
@@ -20,6 +38,19 @@ function App() {
       <h2>{nm}</h2>
 
       <h2>{welcome("Nerd")}</h2>
+
+      {list.map((item) => {
+        return (
+          <div key={item.objectID}>
+            <p>{item.title}</p>
+            <a href={item.url}>{item.title}</a>
+            <p>{item.author}</p>
+            <p>{item.num_comments}</p>
+            <p>{item.points}</p>
+            <hr />
+          </div>
+        );
+      })}
     </div>
   );
 }
