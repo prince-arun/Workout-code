e6a3a733f8b1d2ebbd296a0adbebdb88f504c80f
prince-arun <prince.skillsafari@gmail.com>
2023-09-02 23:54:35 +0530

    Filesystem - writeFile and append files

--- 05.Node/03.Filesystem/server.js
+++ 05.Node/03.Filesystem/server.js
@@ -1,6 +1,8 @@
 var fs = require("fs");
 var path = require("path");
 
+//readFile modules in filesystem
+
 fs.readFile(
   path.join(__dirname, "A.file", "text.txt"),
   "utf-8",
@@ -11,6 +13,37 @@ fs.readFile(
   }
 );
 
+//writeFile modules & appending
+fs.writeFile(
+  path.join(__dirname, "A.file", "new.txt"),
+  "write filesystem completed",
+  (err) => {
+    if (err) throw err;
+    console.log("write completed");
+    fs.appendFile(
+      path.join(__dirname, "A.file", "new.txt"),
+      "\nsecond line write completed",
+      (err) => {
+        if (err) throw err;
+        console.log("append completed");
+      }
+    );
+  }
+);
+
+//writeFiles in loop
+for (let i = 0; i < 3; i++) {
+  fs.appendFile(
+    path.join(__dirname, "A.file", "loop.txt"),
+    "\nhello, world",
+    (err) => {
+      if (err) throw err;
+      console.log("loop completed");
+    }
+  );
+}
+
+//========================================================================================//
 //catching errors
 
 process.on("uncaughtException", (err) => {
