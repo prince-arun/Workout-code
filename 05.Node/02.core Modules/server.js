a8b2ed305c7d408b41c0ef912a9ecb43c0504db4
prince-arun <prince.skillsafari@gmail.com>
2023-09-02 22:30:36 +0530

    import and export in node

--- 05.Node/02.core Modules/server.js
+++ 05.Node/02.core Modules/server.js
@@ -29,3 +29,19 @@ console.log(path.basename(__filename));
 console.log(path.dirname(__filename));
 console.log(path.extname(__filename));
 console.log(path.parse(__filename).root);
+
+//=====================================================
+//importing from another file
+console.log("==========================calc======================");
+
+//const calc = require("./data");
+
+// console.log(calc.add(23, 33));
+// console.log(calc.sub(23, 33));
+// console.log(calc.mul(23, 33));
+// console.log(calc.div(23, 33));
+
+//we also can destructure
+const { add, sub, mul, div } = require("./data");
+
+console.log(add(34, 44));
---
+++ 05.Node/02.core Modules/server.mjs
@@ -0,0 +1,13 @@
+import { add, sub, mul, div } from "./data.js";
+
+// import { version, type } from "os";
+
+// console.log(version());
+
+import os from "os";
+console.log(os.version());
+
+console.log(add(3, 3));
+console.log(sub(3, 3));
+console.log(mul(3, 3));
+console.log(div(3, 3));
