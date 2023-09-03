300e76154b7dd1ea6cffcac12de46acfae736f07
prince-arun <prince.skillsafari@gmail.com>
2023-09-03 22:34:03 +0530

    sending files as response

--- 06.Express/02.Intro/server.js
+++ 06.Express/02.Intro/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const PORT = 5000;
 
@@ -6,8 +7,32 @@ const PORT = 5000;
 const app = express();
 
 //starting a server
-app.get("/", (req, res) => {
-  res.send(`<h1>Welcome to Express.js</h1>`);
+// app.get("/", (req, res) => {
+//   res.send(`<h1>Welcome to Express.js</h1>`);
+// });
+
+//sending files
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
+
+app.get("/about(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+//using reg expression to handle error
+app.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+//redirecting page permanently
+app.get("/intro", (req, res) => {
+  res.redirect(301, "/about");
+});
+
+//managing errors
+app.all("*", (req, res) => {
+  res.status(404).send(`<h1> Page Not found </h1>`);
 });
 
 //listening to the server
